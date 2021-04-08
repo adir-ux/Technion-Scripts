2b4466c30cf6b6b09c242e6a005fe1dc0ca706bd
AdirBetite <55162287+adir-ux@users.noreply.github.com>
2021-04-08 20:18:34 +0300

    Create clone_pull_git.sh

---
+++ General/clone_pull_git.sh
@@ -0,0 +1,6 @@
+# Try to clone $repo_addr. if $repo_name folder already exists, pull instead
+#
+# $repo_name: the name of the repository
+# $repo_addr: "https://adir-ux@github.com/adir-ux/$repo_name"
+#
+git clone $repo_addr || (cd $repo_folder ; git pull)
